2fdf89e708910531a640dadc049874b4d709c4b3
rayankavishka <rayanpersonaluse2004@gmail.com>
2026-08-08 10:27:38 +0530

    Initial project

---
+++ src/main/java/lk/ijse/rayvora/RayvoraApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.rayvora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RayvoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RayvoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/rayvora/RayvoraApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.rayvora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RayvoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
